0312d4f4ee55ebbc6c4e98e16938a2e67fab90d7
Barranger Ridler <barranger@redbitdev.com>
2014-09-10 16:39:41 -0400

    First cut of the Photos page

--- scripts/viewmodels/photos.js
+++ scripts/viewmodels/photos.js
@@ -7,8 +7,18 @@ var app = app || {};
 app.Photos = (function () {
     'use strict';
 
+    var ds = new kendo.data.DataSource({
+        transport: {
+            read: {
+                url: "data/photos.json",
+                dataType: "json"
+            }
+        }
+    });
+
     return new kendo.observable({
-        title: "Photos"
+        title: "Photos",
+        dataSource: ds
     });
 
 }());
