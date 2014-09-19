833762b5b7f479e4fa3ae39ba9a4b359efe99b5e
Barranger Ridler <barranger@redbitdev.com>
2014-09-19 10:47:55 -0400

    Added more styling and a couple of images to better match the styling from the mockups (not complete yet)

--- app/scripts/viewmodels/menu.js
+++ app/scripts/viewmodels/menu.js
@@ -17,7 +17,15 @@
         });
 
         var changeView = function (e) {
-            console.log("View Changed", e.currentTarget.text);
+            e.preventDefault();
+            var icon = $('#view-changer .km-icon');
+            if (icon.hasClass('km-th-large')) {
+                icon.removeClass('km-th-large').addClass('km-list-bullet');
+                $("#popular-list").attr('data-template', 'photoMenuItem');
+            } else {
+                icon.removeClass('km-list-bullet').addClass('km-th-large');
+                $("#popular-list").attr('data-template', 'menuItem');
+            }
         };
 
         return new kendo.observable({
