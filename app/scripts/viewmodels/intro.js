a0f060071e12bee0cb11ec43393496343b7424c8
Barranger Ridler <barranger@redbitdev.com>
2014-10-10 16:11:24 +0000

    Implmented sliding navigation on the intro screens

---
+++ app/scripts/viewmodels/intro.js
@@ -0,0 +1,35 @@
+/*jslint browser: true */
+/*global app, kendo */
+"use strict";
+
+(function (win) {
+    win.app = win.app || {};
+    
+    win.app.Intro = kendo.observable({
+        handleSwipe: function (e) {
+            if( e.direction === "right" ) {
+                switch( e.sender.element[0].id) {
+                    case "img2":
+                        app.mobileApp.navigate( "#intro-1", "slide:right");
+                        break;
+                    case "img3":
+                        app.mobileApp.navigate( "#intro-2", "slide:right");
+                        break;
+                }
+            } else {
+                switch( e.sender.element[0].id) {
+                    case "img1":
+                        app.mobileApp.navigate( "#intro-2", "slide:left");
+                        break;
+                    case "img2":
+                        app.mobileApp.navigate( "#intro-3", "slide:left");
+                        break;
+                    case "img3":
+                        app.mobileApp.navigate( "views/menu.html", "slide:left");
+                        break;
+                }
+            }
+            
+        }
+    });
+}(window));
