9fccf3f79c509031032dcf387ccc0c06e45ae362
Barranger Ridler <barranger@redbitdev.com>
2014-10-21 17:09:25 +0000

    Fixed a typeo

--- app/scripts/viewmodels/edit-profile.js
+++ app/scripts/viewmodels/edit-profile.js
@@ -56,8 +56,8 @@
         updateProfile: function (e) {
             e.preventDefault();
             var url = "https://194-TGP-611.mktorest.com/"; //rest?email=" + this.profile.email + "&access_token=" + this.profile.access_token;
-            c
-            if (this.profile.fistName == "" || this.profile.lastName == "" || this.profile.email == "") {
+            
+            if (this.profile.firstName === "" || this.profile.lastName === "" || this.profile.email === "") {
                 win.app.alert("You must complete all fields to participate in the contest");
             } else if (win.navigator.simulator) {
                 win.app.alert("Sorry, but you must run this on the device to recieve the prize");
